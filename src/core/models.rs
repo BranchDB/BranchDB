fca524fab980b0b4e180c0cb09f30fade1f0fbfb
dibyadeepsaha <saha18@illinois.edu>
2025-07-15 14:16:41 -0500

    Current semi working version before MAJOR CHANGES INCOMING :)

--- src/core/models.rs
+++ src/core/models.rs
@@ -2,25 +2,43 @@ use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum StructuredValue {
+    Map(HashMap<String, serde_json::Value>),
+    Array(Vec<serde_json::Value>),
+    Primitive(serde_json::Value),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum CrdtValue {
+    Counter(u64),
+    Register(StructuredValue),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Commit {
     pub parents: Vec<[u8; 32]>,
     pub message: String,
     pub timestamp: u64,
     pub changes: Vec<Change>,
-    pub tree: HashMap<String, [u8; 32]>, 
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct CommitMetadata {
-    pub branches: HashMap<String, [u8; 32]>, 
-    pub head: [u8; 32],                    
+    pub tree: HashMap<String, [u8; 32]>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Change {
-    Insert { table: String, id: String, value: Vec<u8> },
-    Update { table: String, id: String, value: Vec<u8> },
-    Delete { table: String, id: String },
+    Insert { 
+        table: String, 
+        id: String, 
+        value: Vec<u8> 
+    },
+    Update { 
+        table: String, 
+        id: String, 
+        value: Vec<u8> 
+    },
+    Delete { 
+        table: String, 
+        id: String 
+    },
 }
 
 impl Change {
